15e0380422c0c01742ec9f09e09a7cb3138d9a36
MuruGeshhh <108484042+MuruGeshhh@users.noreply.github.com>
2022-08-06 19:00:08 +0800

    trial 1

---
+++ src/main/java/vttp2022/ssf/day15_weather/Day15WeatherApplication.java
@@ -0,0 +1,13 @@
+package vttp2022.ssf.day15_weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day15WeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day15WeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022/ssf/day15_weather/Day15WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.ssf.day15_weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day15WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
